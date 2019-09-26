88c0eebbc7fdfa618670c6ca70757ad91f6f58dc
micher94 <55001134+micher94@users.noreply.github.com>
2019-09-26 16:26:59 -0400

    Add files via upload
    fixed print thing

--- Twitter_Updated.py
+++ Twitter_Updated.py
@@ -32,7 +32,7 @@ def readtweets(searchterm, searchtype, consumer_key, consumer_secret):
                    
             for tweets in api.user_timeline(screen_name=sn, result_type = 'popular', tweet_mode = 'extended', count = 5):
                 tweetlist.append(tweets.full_text)
-            print(tweetlist)
+
         else:
 # used https://stackoverflow.com/questions/14856526/parsing-twitter-json-object-in-python to figure out how to pull ONLY the text from the file
 # leveraged https://stackoverflow.com/questions/38872195/tweepy-exclude-retweets for how to exclude retweets from twitter list and eliminate duplicates
